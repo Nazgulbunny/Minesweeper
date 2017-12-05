111b794531c209058f3c81df3125220d22e1fa98
Nazgulbunny <maxsermi@gmail.com>
2017-12-05 19:38:50 +0100

    added third variable for the bomb tile

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,10 @@
 
 const blankline = '  |   |  ';
+//guessLine represents what the board will look like when a player guesses by "clicking" (selecting) the first square of this row.
 const guessLine = '1 |   |  ';
-
+//bombLine represents what the board will look like when a player clicks and reveals a bomb.
+const bombLine = '  | B |  '
+//for testing purpouses
 console.log("This is what an empty board would look like:");
 
 console.log(blankline); 
