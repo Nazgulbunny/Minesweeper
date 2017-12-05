9946e01cf12e5d2e5ae43031e8befb28fbe97dbd
Nazgulbunny <maxsermi@gmail.com>
2017-12-05 19:44:11 +0100

    Hardcoded and testing

--- minesweeper.js
+++ minesweeper.js
@@ -1,5 +1,5 @@
 
-const blankline = '  |   |  ';
+const blankLine = '  |   |  ';
 //guessLine represents what the board will look like when a player guesses by "clicking" (selecting) the first square of this row.
 const guessLine = '1 |   |  ';
 //bombLine represents what the board will look like when a player clicks and reveals a bomb.
@@ -7,7 +7,14 @@ const bombLine = '  | B |  '
 //for testing purpouses
 console.log("This is what an empty board would look like:");
 
-console.log(blankline); 
-console.log(blankline); 
-console.log(blankline);
+console.log(blankLine); 
+console.log(blankLine); 
+console.log(blankLine);
+
+//again testing purpouse
+console.log("This is what a board with a guess and a bomb on it would look like:");
+
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
 
