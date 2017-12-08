a8d8157550dd7c06e2085661d433fd8e4aaa523e
Nazgulbunny <maxsermi@gmail.com>
2017-12-08 16:33:43 +0100

    Last bugs fixed

--- minesweeper.js
+++ minesweeper.js
@@ -6,9 +6,9 @@ const testBoard = [
 
 const printBoard = board =>{
     console.log("Current Board: ");
-    printBoard(board[0].join(' | '));
-	printBoard(board[1].join(' | '));
-	printBoard(board[2].join(' | '));
+    console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
 
 };
 
