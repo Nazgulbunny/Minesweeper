70b12bafe01af8b317d937fe97a9149349da7569
Nazgulbunny <maxsermi@gmail.com>
2017-12-08 16:16:15 +0100

    Made a function to create the board and print it out

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,19 @@
-const board = [
-[" ", " ", " "],
-[" ", " ", " "],
-[" ", " ", " "]
+const testBoard = [
+	[" ", " ", " "],
+	[" ", " ", " "],
+	[" ", " ", " "]
 ];
 
-console.log(board);
+const printBoard = board =>{
+    console.log("Current Board:");
+    printBoard(board[0].join(' | ');
+	printBoard(board[1].join(' | ');
+	printBoard(board[2].join(' | ');
+
+};
+
+printBoard(testBoard);
+board[0][1] = "1";
+board[1][0] = "B";
+
+printBoard(testBoard);
