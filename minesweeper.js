b23a4f080cf90f3c4feabfa4f0591f4a9015ef83
Nazgulbunny <maxsermi@gmail.com>
2017-12-08 15:17:09 +0100

    remove the hard coded code to make it more dinamic

--- minesweeper.js
+++ minesweeper.js
@@ -1,20 +1,2 @@
 
-const blankLine = '  |   |  ';
-//guessLine represents what the board will look like when a player guesses by "clicking" (selecting) the first square of this row.
-const guessLine = '1 |   |  ';
-//bombLine represents what the board will look like when a player clicks and reveals a bomb.
-const bombLine = '  | B |  '
-//for testing purpouses
-console.log("This is what an empty board would look like:");
-
-console.log(blankLine); 
-console.log(blankLine); 
-console.log(blankLine);
-
-//again testing purpouse
-console.log("This is what a board with a guess and a bomb on it would look like:");
-
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
 
