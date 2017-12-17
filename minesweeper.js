788c47b6dcd08000e1b33f05f8788676820547ce
Nazgulbunny <maxsermi@gmail.com>
2017-12-17 17:19:04 +0100

    bomb function added and code refactor

--- minesweeper.js
+++ minesweeper.js
@@ -18,18 +18,43 @@ testBoard[1][0] = "B";
 
 printBoard(testBoard);*/
 
-const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
-const board = [];
-for (let i = 0; i < numberOfRows; i++) {
-	const row = [];
-	for (let j = 0; j < numberOfColumns; j++) {
-	row.push(" ");
-	}
-	board.push(row);
-}
-
-return(board);
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    const board = [];
+    for (i = 0; i < numberOfRows; i++) {
+        const row = [];
+        for (j = 0; j < numberOfColumns; j++) {
+            row.push(" ");
+        }
+        board.push(row);
+    }
+    return board;
 };
 
 
-consolle.log(generatePlayerBoard(5,5));
+console.log(generatePlayerBoard(2, 2));
+
+
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+
+    const board = [];
+    for (var i = 0; i < numberOfRows; i++) {
+        const row = [];
+        for (var j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
+    var numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        //The code in this while loop has the potential to place bombs on top of already existing bombs. This will be fixed later withcontrol flow.
+        var randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        var randomColumnIndex = Math.floor(Math.random() * numberOfRows);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+
+    return board;
+
+};
+console.log(generateBombBoard(2, 2 , 3));
